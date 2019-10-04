ac31fdb0698d893164da69d1831501d20d762456
Daniel Coto <daniel_coto@live.com>
2019-10-03 22:16:34 -0400

    fix: remove todo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,23 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// TODO: Add lazy loaded routes
 const appRoutes: Routes = [
     { path: '', redirectTo: '/feeds', pathMatch: 'full' },
-    { path: 'feeds', loadChildren: () => import('./feeds/feeds.module').then(m => m.FeedsModule) },
-    { path: 'downloads', loadChildren: () => import('./downloads/downloads.module').then(m => m.DownloadsModule) },
-    { path: 'requests', loadChildren: () => import('./requests/requests.module').then(m => m.RequestsModule) },
+    {
+        path: 'feeds',
+        loadChildren: () =>
+            import('./feeds/feeds.module').then(m => m.FeedsModule),
+    },
+    {
+        path: 'downloads',
+        loadChildren: () =>
+            import('./downloads/downloads.module').then(m => m.DownloadsModule),
+    },
+    {
+        path: 'requests',
+        loadChildren: () =>
+            import('./requests/requests.module').then(m => m.RequestsModule),
+    },
 ];
 
 @NgModule({
